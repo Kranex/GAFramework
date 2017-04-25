fad8a8d3b4a959f7f303ccde41963cfd46b548d6
Kranex <oliverstrik@gmail.com>
2017-04-25 22:37:19 +0200

    First 'Working' version
    First theoretically working GAFramework. Testing with Framework Scripts.

---
+++ Basic_TSP/Basic_TSP.js
@@ -0,0 +1,39 @@
+//statement and table initalisations.
+var statement;
+var table;
+
+
+// init data function.
+function init(){
+	statement = db.createStatement();
+	table = statement.executeQuery("SELECT * FROM CITIES");
+}
+
+// validity function.
+function validate(){
+	
+}
+
+// init pool function.
+function initPool(poolSize){
+	
+}
+
+//fitness function.
+function fitness(){
+	
+}
+
+// breed function.
+function breed(){
+	
+}
+
+// mutate function.
+function mutate(){
+	
+}
+
+// end function for output.
+function output(){
+}
